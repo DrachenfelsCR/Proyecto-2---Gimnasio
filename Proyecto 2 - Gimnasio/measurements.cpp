f5c9cf0c2fb78b342f5b60990c4920df18164842
David Quesada Romero <davidquesadaromero@gmail.com>
2019-10-21 19:55:16 -0600

    Completada clase measurements

--- Proyecto 2 - Gimnasio/measurements.cpp
+++ Proyecto 2 - Gimnasio/measurements.cpp
@@ -7,3 +7,34 @@ measurements::measurements() {
 	this->body_mass = 0;
 }
 
+void measurements::setWeight(double weight) {
+	this->weight = weight;
+}
+
+void measurements::setHeight(double height) {
+	this->height = height;
+}
+
+void measurements::setBodyFat(double body_fat) {
+	this->body_fat = body_fat;
+}
+
+void measurements::setBodyMass(double body_mass) {
+	this->body_mass = body_mass;
+}
+
+double measurements::getWeight() {
+	return this->weight;
+}
+
+double measurements::getHeight() {
+	return this->height;
+}
+
+double measurements::getBodyFat() {
+	return this->body_fat;
+}
+
+double measurements::getBodyMass() {
+	return this->body_mass;
+}
