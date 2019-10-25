fe970d5ecdf935fc9f6c7d18b72036cc12ff693e
David Quesada Romero <davidquesadaromero@gmail.com>
2019-10-24 23:50:19 -0600

    Agregada clase schedule (Horario)

---
+++ Proyecto 2 - Gimnasio/schedule.cpp
@@ -0,0 +1,465 @@
+#include "schedule.h"
+
+schedule::schedule()
+{
+	for (int i = 2; i < 15; i++)
+		for (int j = 0; j < 7; j++)
+			this->the_schedule[i][j] = "";
+	this->the_schedule[0][0] = "      ";
+	this->the_schedule[0][1] = "  Lunes(1) |\t";
+	this->the_schedule[0][2] = " Martes(2) |";
+	this->the_schedule[0][3] = " Miercoles(3) |";
+	this->the_schedule[0][4] = "  Jueves(4) |";
+	this->the_schedule[0][5] = " Viernes(5) |\t";
+	this->the_schedule[0][6] = " Sabado(6) |";
+	//----------------------------------
+	this->the_schedule[1][0] = "6:00 | ";
+	this->the_schedule[2][0] = "7:00 | ";
+	this->the_schedule[3][0] = "8:00 | ";
+	this->the_schedule[4][0] = "9:00 | ";
+	this->the_schedule[5][0] = "10:00| ";
+	this->the_schedule[6][0] = "11:00| ";
+	this->the_schedule[7][0] = "12:00| ";
+	this->the_schedule[8][0] = "13:00| ";
+	this->the_schedule[9][0] = "14:00| ";
+	this->the_schedule[10][0] = "15:00| ";
+	this->the_schedule[11][0] = "16:00| ";
+	this->the_schedule[12][0] = "17:00| ";
+	this->the_schedule[13][0] = "18:00| ";
+	this->the_schedule[14][0] = "19:00| ";
+}
+
+string schedule::toString() {
+	stringstream s;
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 7; j++)
+			if (i == 0) {
+				s << setw(10) << this->the_schedule[i][j];
+			}
+			else if (j == 0)
+			{
+				s << this->the_schedule[i][j];
+			}
+			else {
+				s << setw(14) << this->the_schedule[i][j];
+			}
+		s << endl;
+	}
+	s << endl;
+	return s.str();
+}
+
+string schedule::getElement(int i, int j) {
+	return this->the_schedule[i][j];
+}
+
+void schedule::setPosition(int i, int j, string element) {
+	this->the_schedule[i][j] = element;
+}
+
+bool schedule::insertElement(int day, string time, string element) {
+	string aux;
+	if ((day > 6) || (day < 1))
+	{
+		return false;
+	}
+	else
+	{
+		switch (day)
+		{
+		case 1:
+			if (time == "6:00") {
+				aux = element;
+				this->the_schedule[1][day] = element;
+			}
+			else if (time == "7:00")
+			{
+				aux = element;
+				this->the_schedule[2][day] = element;
+			}
+			else if (time == "8:00")
+			{
+				aux = element;
+				this->the_schedule[3][day] = element;
+			}
+			else if (time == "9:00")
+			{
+				aux = element;
+				this->the_schedule[4][day] = element;
+			}
+			else if (time == "10:00")
+			{
+				aux = element;
+				this->the_schedule[5][day] = element;
+			}
+			else if (time == "11:00")
+			{
+				aux = element;
+				this->the_schedule[6][day] = element;
+			}
+			else if (time == "12:00")
+			{
+				aux = element;
+				this->the_schedule[7][day] = element;
+			}
+			else if (time == "13:00")
+			{
+				aux = element;
+				this->the_schedule[8][day] = element;
+			}
+			else if (time == "14:00")
+			{
+				aux = element;
+				this->the_schedule[9][day] = element;
+			}
+			else if (time == "15:00")
+			{
+				aux = element;
+				this->the_schedule[10][day] = element;
+			}
+			else if (time == "16:00")
+			{
+				aux = element;
+				this->the_schedule[11][day] = element;
+			}
+			else if (time == "17:00")
+			{
+				aux = element;
+				this->the_schedule[12][day] = element;
+			}
+			else if (time == "18:00")
+			{
+				aux = element;
+				this->the_schedule[13][day] = element;
+			}
+			else if (time == "19:00")
+			{
+				aux = element;
+				this->the_schedule[14][day] = element;
+			}
+			else
+			{
+				aux = element;
+				return false;
+			}
+		case 2:
+			if (time == "6:00") {
+				aux = element;
+				this->the_schedule[1][day] = element;
+			}
+			else if (time == "7:00")
+			{
+				aux = element;
+				this->the_schedule[2][day] = element;
+			}
+			else if (time == "8:00")
+			{
+				aux = element;
+				this->the_schedule[3][day] = element;
+			}
+			else if (time == "9:00")
+			{
+				aux = element;
+				this->the_schedule[4][day] = element;
+			}
+			else if (time == "10:00")
+			{
+				aux = element;
+				this->the_schedule[5][day] = element;
+			}
+			else if (time == "11:00")
+			{
+				aux = element;
+				this->the_schedule[6][day] = element;
+			}
+			else if (time == "12:00")
+			{
+				aux = element;
+				this->the_schedule[7][day] = element;
+			}
+			else if (time == "13:00")
+			{
+				aux = element;
+				this->the_schedule[8][day] = element;
+			}
+			else if (time == "14:00")
+			{
+				aux = element;
+				this->the_schedule[9][day] = element;
+			}
+			else if (time == "15:00")
+			{
+				aux = element;
+				this->the_schedule[10][day] = element;
+			}
+			else if (time == "16:00")
+			{
+				aux = element;
+				this->the_schedule[11][day] = element;
+			}
+			else if (time == "17:00")
+			{
+				aux = element;
+				this->the_schedule[12][day] = element;
+			}
+			else if (time == "18:00")
+			{
+				aux = element;
+				this->the_schedule[13][day] = element;
+			}
+			else if (time == "19:00")
+			{
+				aux = element;
+				this->the_schedule[14][day] = element;
+			}
+			else
+			{
+				return false;
+			}
+		case 3:
+			if (time == "6:00")
+				this->the_schedule[1][day] = element;
+			else if (time == "7:00")
+			{
+				this->the_schedule[2][day] = element;
+			}
+			else if (time == "8:00")
+			{
+				this->the_schedule[3][day] = element;
+			}
+			else if (time == "9:00")
+			{
+				this->the_schedule[4][day] = element;
+			}
+			else if (time == "10:00")
+			{
+				this->the_schedule[5][day] = element;
+			}
+			else if (time == "11:00")
+			{
+				this->the_schedule[6][day] = element;
+			}
+			else if (time == "12:00")
+			{
+				this->the_schedule[7][day] = element;
+			}
+			else if (time == "13:00")
+			{
+				this->the_schedule[8][day] = element;
+			}
+			else if (time == "14:00")
+			{
+				this->the_schedule[9][day] = element;
+			}
+			else if (time == "15:00")
+			{
+				this->the_schedule[10][day] = element;
+			}
+			else if (time == "16:00")
+			{
+				this->the_schedule[11][day] = element;
+			}
+			else if (time == "17:00")
+			{
+				this->the_schedule[12][day] = element;
+			}
+			else if (time == "18:00")
+			{
+				this->the_schedule[13][day] = element;
+			}
+			else if (time == "19:00")
+			{
+				this->the_schedule[14][day] = element;
+			}
+			else
+			{
+				return false;
+			}
+		case 4:
+			if (time == "6:00")
+				this->the_schedule[1][day] = element;
+			else if (time == "7:00")
+			{
+				this->the_schedule[2][day] = element;
+			}
+			else if (time == "8:00")
+			{
+				this->the_schedule[3][day] = element;
+			}
+			else if (time == "9:00")
+			{
+				this->the_schedule[4][day] = element;
+			}
+			else if (time == "10:00")
+			{
+				this->the_schedule[5][day] = element;
+			}
+			else if (time == "11:00")
+			{
+				this->the_schedule[6][day] = element;
+			}
+			else if (time == "12:00")
+			{
+				this->the_schedule[7][day] = element;
+			}
+			else if (time == "13:00")
+			{
+				this->the_schedule[8][day] = element;
+			}
+			else if (time == "14:00")
+			{
+				this->the_schedule[9][day] = element;
+			}
+			else if (time == "15:00")
+			{
+				this->the_schedule[10][day] = element;
+			}
+			else if (time == "16:00")
+			{
+				this->the_schedule[11][day] = element;
+			}
+			else if (time == "17:00")
+			{
+				this->the_schedule[12][day] = element;
+			}
+			else if (time == "18:00")
+			{
+				this->the_schedule[13][day] = element;
+			}
+			else if (time == "19:00")
+			{
+				this->the_schedule[14][day] = element;
+			}
+			else
+			{
+				return false;
+			}
+		case 5: if (time == "6:00")
+			this->the_schedule[1][day] = element;
+			  else if (time == "7:00")
+		{
+			this->the_schedule[2][day] = element;
+		}
+			  else if (time == "8:00")
+		{
+			this->the_schedule[3][day] = element;
+		}
+			  else if (time == "9:00")
+		{
+			this->the_schedule[4][day] = element;
+		}
+			  else if (time == "10:00")
+		{
+			this->the_schedule[5][day] = element;
+		}
+			  else if (time == "11:00")
+		{
+			this->the_schedule[6][day] = element;
+		}
+			  else if (time == "12:00")
+		{
+			this->the_schedule[7][day] = element;
+		}
+			  else if (time == "13:00")
+		{
+			this->the_schedule[8][day] = element;
+		}
+			  else if (time == "14:00")
+		{
+			this->the_schedule[9][day] = element;
+		}
+			  else if (time == "15:00")
+		{
+			this->the_schedule[10][day] = element;
+		}
+			  else if (time == "16:00")
+		{
+			this->the_schedule[11][day] = element;
+		}
+			  else if (time == "17:00")
+		{
+			this->the_schedule[12][day] = element;
+		}
+			  else if (time == "18:00")
+		{
+			this->the_schedule[13][day] = element;
+		}
+			  else if (time == "19:00")
+		{
+			this->the_schedule[14][day] = element;
+		}
+			  else
+		{
+			return false;
+		}
+		case 6: if (time == "6:00")
+			this->the_schedule[1][day] = element;
+			  else if (time == "7:00")
+		{
+			this->the_schedule[2][day] = element;
+		}
+			  else if (time == "8:00")
+		{
+			this->the_schedule[3][day] = element;
+		}
+			  else if (time == "9:00")
+		{
+			this->the_schedule[4][day] = element;
+		}
+			  else if (time == "10:00")
+		{
+			this->the_schedule[5][day] = element;
+		}
+			  else if (time == "11:00")
+		{
+			this->the_schedule[6][day] = element;
+		}
+			  else if (time == "12:00")
+		{
+			this->the_schedule[7][day] = element;
+		}
+			  else if (time == "13:00")
+		{
+			this->the_schedule[8][day] = element;
+		}
+			  else if (time == "14:00")
+		{
+			this->the_schedule[9][day] = element;
+		}
+			  else if (time == "15:00")
+		{
+			this->the_schedule[10][day] = element;
+		}
+			  else if (time == "16:00")
+		{
+			this->the_schedule[11][day] = element;
+		}
+			  else if (time == "17:00")
+		{
+			this->the_schedule[12][day] = element;
+		}
+			  else if (time == "18:00")
+		{
+			this->the_schedule[13][day] = element;
+		}
+			  else if (time == "19:00")
+		{
+			this->the_schedule[14][day] = element;
+		}
+			  else
+		{
+			return false;
+		}
+
+		default:
+			return false;
+			break;
+		}
+	}
+}
+
+schedule::~schedule()
+{
+
+}
+
---
+++ Proyecto 2 - Gimnasio/schedule.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+class schedule
+{
+private:
+	string the_schedule[15][7];
+
+public:
+	schedule();
+	~schedule();
+	void setPosition(int, int, string);
+	string getElement(int, int);
+	string toString();
+	bool insertElement(int, string, string);
+};
